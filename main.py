0020619738b09e1e07da9ea86b4b8d9a13de0bf8
ArasCha <araschaignepro@gmail.com>
2025-02-17 16:10:08 +0100

    [ADD] cities where to explore profiles

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from request import request_profiles, request_profile_sections, request_profile_connex_profiles, notify_error
-from utils import extract_profile_id, extract_profiles_info, filter_connex_profiles, format_data
+from utils import extract_profile_id, extract_profiles_info, filter_profiles, format_data
 from db import insert_profile, get_list_of_users_id, get_index, update_index
 import time
 import traceback
@@ -15,9 +15,13 @@ def add_profile_to_db(profile_id: str, profile_url: str):
 
 
 
+# search terms to add: quant, trader, trading, algorithmic trader, quant dev, quantitative developer, quantitative risk, risk quant, analyste quantitatif, quant analyst, quant trader, quant researcher, quant trader 
 search_terms = ["quantitative%20research", "quantitative%20researcher", "quantitative%20trading", "quantitative%20trader", "quantitative%20analyst"]
 circles = ["S", "O"]
-city_ids = [104406358,102436504,103291313,102277331,103112676,102454443,106383538,90009496,90000070]
+# city_ids = [104406358,102436504,103291313,102277331,103112676,102454443,106383538,90009496,90000070]
+
+# shanghai, pékin, amsterdam, shenzhen, tokyo, Mumbai, Boston, Sydney, Toronto, Zug (suisse), Dubai, Casablanca, Abu Dhabi, Montreal, Francfort, Dublin, Rotterdam, Munich, Lausanne, Brusselles
+city_ids = [108025228,90010386,104738515,106204383,101728226,103606803,100025096,104769905,102380872,106164952,106750182,90010383,103873152,103925994,102772228]
 
 if __name__ == "__main__":
 
@@ -25,14 +29,14 @@ if __name__ == "__main__":
         for circle in circles:
             for city_id in city_ids:
 
+                print(search_term, circle, city_id)
+
                 for page_number in range(100): # only 100 pages maximum
-
-                    data = request_profiles(page_number, search_term, city_id, circle)
-
-                    profiles = data["included"]
                     print(page_number)
 
-                    for profile in profiles:
+                    profiles = request_profiles(page_number, search_term, city_id, circle)
+
+                    for profile in filter_profiles(profiles):
                         try:
                             try:
                                 raw_profile_url: str = profile["navigationUrl"]
@@ -61,9 +65,8 @@ if __name__ == "__main__":
 
     #         raw_connex_profiles = request_profile_connex_profiles(root_profile_id)
     #         connex_profiles = extract_profiles_info(raw_connex_profiles)
-    #         filtered_connex_profiles = filter_connex_profiles(connex_profiles)
 
-    #         for profile in filtered_connex_profiles:
+    #         for profile in filter_profiles(connex_profiles):
     #             if profile["id"] not in get_list_of_users_id():
     #                 add_profile_to_db(profile["id"], profile["url"])
     #                 print(profile["url"])
